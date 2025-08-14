de837e0a6068560cfb1707f6ab3664be4c241786
Chronoss0518 <sinyusuke5000@gmail.com>
2025-08-14 15:20:18 +0900

    DrawImageもmainFunctionにベタ打ち

--- src/DrawImage/DrawImage.h
+++ src/DrawImage/DrawImage.h
@@ -1,17 +1,80 @@
 #pragma once
+#include"../main.h"
 
-class DrawImage : public ApplicationBase
+static constexpr
+#ifdef _UNICODE
+wchar_t
+#else
+char
+#endif
+const* TEXTURE_FILE_NAME =
+#ifdef _UNICODE
+L"Test.bmp";
+#else
+"Test.bmp";
+#endif
+
+static constexpr
+#ifdef _UNICODE
+wchar_t
+#else
+char
+#endif
+const* APPLICATION_TITLE =
+#ifdef _UNICODE
+L"Quick Starter - DrawTexture";
+#else
+"Quick Starter - DrawTexture";
+#endif
+
+int mainFunction(HINSTANCE _hIns)
 {
-public:
+    //"CreateWindow.h"‚ğQl‚É‚µ‚Ä‚­‚¾‚³‚¢//
+    ChWin::WindClassObject classObject;
 
-	void Init(HINSTANCE)override;
+    classObject.Init();
+    classObject.SetInstance(_hIns);
+    classObject.SetBackgroundColor((HBRUSH)GetStockObject(BLACK_BRUSH));
+    classObject.SetCursol(LoadCursor(NULL, IDC_ARROW));
+    classObject.RegistClass(WIND_CLASS_NAME);
 
-	void Update()override;
+    ChWin::WindObject windObject;
+    {
+        auto&& windSize = ChWin::GetScreenSize();
+        ChWin::WindCreater creater = ChWin::WindCreater(_hIns);
+        creater.SetInitSize(windSize);
+        ChWin::WindStyle style;
+        style.AddOverlappedWindow();
+        creater.SetWindStyle(&style);
 
-	void Release()override;
 
-private:
-	CreateApplicationWindow applicationBase;
+        creater.Create(windObject, APPLICATION_TITLE, classObject.GetWindClassName());
 
-	ChWin::Texture texture;
-};
+        windObject.SetWindProcedure(WM_DESTROY, [&](HWND _hWnd, UINT _uMsg, WPARAM _wParam, LPARAM _lParam)->LRESULT {
+            PostQuitMessage(0);
+            windObject.Release();
+            return 0;
+            });
+    }
+
+    //‰æ‘œ‚ğì¬//
+    ChWin::Texture texture;
+
+    texture.CreateTexture(_hIns, TEXTURE_FILE_NAME);
+
+    while (windObject.Update())
+    {
+        auto&& dc = windObject.DrawStart();
+
+        auto&& size = texture.GetTextureSize();
+        //‰æ‘œ‚ğ0,0ˆÊ’u‚É•`‰æ//
+        texture.Draw(dc, 0, 0, size.w, size.h);
+
+        windObject.DrawEnd(dc);
+    }
+
+    texture.Release();
+
+    windObject.Release();
+    classObject.Release();
+}
