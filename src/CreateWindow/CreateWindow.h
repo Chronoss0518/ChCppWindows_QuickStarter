5f23c49a9f3561e78d5f68f99ceb82e122520c82
Chronoss0518 <sinyusuke5000@gmail.com>
2025-08-14 15:11:31 +0900

    CreateWindowのmain関数をベタ打ち

--- src/CreateWindow/CreateWindow.h
+++ src/CreateWindow/CreateWindow.h
@@ -1,26 +1,65 @@
 #pragma once
+#include"../main.h"
 
-class CreateApplicationWindow : public ApplicationBase
+static constexpr
+#ifdef _UNICODE
+wchar_t
+#else
+char
+#endif
+const* WIND_CLASS_NAME =
+#ifdef _UNICODE
+L"SampleApplication";
+#else
+"SampleApplication";
+#endif
+
+static constexpr
+#ifdef _UNICODE
+wchar_t
+#else
+char
+#endif
+const* APPLICATION_TITLE =
+#ifdef _UNICODE
+L"Quick Starter - CreateWindow";
+#else
+"Quick Starter - CreateWindow";
+#endif
+
+int main(HINSTANCE _hIns)
 {
-public:
+    ChWin::WindClassObject classObject;
 
-	void Init(HINSTANCE _hIns)override;
+    classObject.Init();
+    //defaultのWindProcedureはChWin::WndProcAまたはChWin::WndProcWになります//
+    classObject.SetInstance(_hIns);
+    classObject.SetBackgroundColor((HBRUSH)GetStockObject(BLACK_BRUSH));
+    classObject.SetCursol(LoadCursor(NULL, IDC_ARROW));
+    classObject.RegistClass(WIND_CLASS_NAME);
 
-	void Init(HINSTANCE _hIns, const std::string& _appName);
+    ChWin::WindObject windObject;
+    {
+        auto&& windSize = ChWin::GetScreenSize();
+        ChWin::WindCreater creater = ChWin::WindCreater(_hIns);
+        creater.SetInitSize(windSize);
+        ChWin::WindStyle style;
+        style.AddOverlappedWindow();
+        creater.SetWindStyle(&style);
 
-	void Update()override;
 
-	bool IsUpdate();
+        creater.Create(windObject, APPLICATION_TITLE, classObject.GetWindClassName());
 
-	void Release()override;
+        //defaultのWindProcedureを利用すると、以下のように必要なWindProcedureを追加することが可能です//
+        windObject.SetWindProcedure(WM_DESTROY, [&](HWND _hWnd, UINT _uMsg, WPARAM _wParam, LPARAM _lParam)->LRESULT {
+            PostQuitMessage(0);
+            windObject.Release();
+            return 0;
+            });
+    }
 
-public:
+    while (windObject.Update()) {}
 
-	ChWin::WindObject& GetWindObject() { return windObject; }
-
-private:
-
-	ChWin::WindObject windObject;
-	ChWin::WindClassObject classObject;
-
-};
+    windObject.Release();
+    classObject.Release();
+}
