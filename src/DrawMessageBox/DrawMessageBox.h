8b83a0d24c89a7056b4985af8db6f572cd586610
Chronoss0518 <sinyusuke5000@gmail.com>
2025-08-14 15:28:59 +0900

    DrawMessageBoxもmainFunctionへベタ打ち

--- src/DrawMessageBox/DrawMessageBox.h
+++ src/DrawMessageBox/DrawMessageBox.h
@@ -1,15 +1,92 @@
 #pragma once
 
-class DrawMessageBox : public ApplicationBase
+static constexpr
+#ifdef _UNICODE
+wchar_t
+#else
+char
+#endif
+const* WIND_CLASS_NAME =
+#ifdef _UNICODE
+L"SampleApplication";
+#else
+"SampleApplication";
+#endif
+
+static constexpr
+#ifdef _UNICODE
+wchar_t
+#else
+char
+#endif
+const* APPLICATION_TITLE =
+#ifdef _UNICODE
+L"Quick Starter - Draw Message Box";
+#else
+"Quick Starter - Draw Message Box";
+#endif
+
+static constexpr
+#ifdef _UNICODE
+wchar_t
+#else
+char
+#endif
+const* TITLE =
+#ifdef _UNICODE
+L"Test";
+#else
+"Test";
+#endif
+
+static constexpr
+#ifdef _UNICODE
+wchar_t
+#else
+char
+#endif
+const* TEXT =
+#ifdef _UNICODE
+L"Open Message Box";
+#else
+"Open Message Box";
+#endif
+
+int mainFunction(HINSTANCE _hIns)
 {
-public:
+    //"CreateWindow.h"‚ğQl‚É‚µ‚Ä‚­‚¾‚³‚¢//
+    ChWin::WindClassObject classObject;
 
-	void Init(HINSTANCE)override;
+    classObject.Init();
+    classObject.SetInstance(_hIns);
+    classObject.SetBackgroundColor((HBRUSH)GetStockObject(BLACK_BRUSH));
+    classObject.SetCursol(LoadCursor(NULL, IDC_ARROW));
+    classObject.RegistClass(WIND_CLASS_NAME);
 
-	void Update()override;
+    ChWin::WindObject windObject;
+    {
+        auto&& windSize = ChWin::GetScreenSize();
+        ChWin::WindCreater creater = ChWin::WindCreater(_hIns);
+        creater.SetInitSize(windSize);
+        ChWin::WindStyle style;
+        style.AddOverlappedWindow();
+        creater.SetWindStyle(&style);
 
-	void Release()override;
 
-private:
-	CreateApplicationWindow applicationBase;
-};
+        creater.Create(windObject, APPLICATION_TITLE, classObject.GetWindClassName());
+
+        windObject.SetWindProcedure(WM_DESTROY, [&](HWND _hWnd, UINT _uMsg, WPARAM _wParam, LPARAM _lParam)->LRESULT {
+            PostQuitMessage(0);
+            windObject.Release();
+            return 0;
+            });
+    }
+
+    //‰æ‘œ‚ğì¬//
+    ChWin::MsgBox messageBox;
+    messageBox.AddDisplayButtonType(ChWin::MsgBox::DisplayButtonType::OkCancel);
+    messageBox.Display(windObject.GethWnd(), TITLE, TEXT);
+
+    windObject.Release();
+    classObject.Release();
+}
