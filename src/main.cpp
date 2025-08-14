754c040431c5316794a228a388c587ff1b5a0f40
Chronoss0518 <sinyusuke5000@gmail.com>
2025-08-14 15:35:02 +0900

    各mainFunctionをコメントアウトで操作するように修正

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,21 @@
-#include"BaseIncluder.h"
-#include"CreateWindow/CreateWindow.h"
+#include"main.h"
 
-#include"DrawImage/DrawImage.h"
-#include"DrawMessageBox/DrawMessageBox.h"
-#include"DrawButton/DrawButton.h"
+//挙動を確認したい項目のコメントアウトを外してください//
+
+//// Windowの作り方//
+//#include"CreateWindow/CreateWindow.h"
+
+//// Bitmap画像のロード及び表示//
+//#include"DrawImage/DrawImage.h"
+
+//// MessageBoxの表示//
+//#include"DrawMessageBox/DrawMessageBox.h"
+
+//// Buttonの作成および表示//
+//#include"DrawButton/DrawButton.h"
+
+//// FileDialogの表示//
 #include"DrawFileDialog/DrawFileDialog.h"
-
-
-//挙動を確認したい項目の#ifまたは#elifの値のみを1に変更してください//
 
 int WinMain(
     HINSTANCE hInstance,
@@ -15,30 +23,5 @@ int WinMain(
     LPSTR     lpCmdLine,
     int       nShowCmd)
 {
-#if 0
-    // Windowの作り方
-    CreateApplicationWindow
-#elif 0
-    // Bitmap画像のロード及び表示
-    DrawImage
-#elif 0
-    // MessageBoxの表示
-    DrawMessageBox
-#elif 0
-    // Buttonの作成および表示
-    DrawButton
-#elif 1
-    // FileDialogの表示
-    DrawFileDialog
-#endif
-
-    application;
-
-    application.Init(hInstance);
-
-    application.Update();
-
-    application.Release();
-
-    return 0;
+    return mainFunction(hInstance);
 }
